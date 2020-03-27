e017bac4c070f49dfa2e42d79cb7687d196d8b17
Vincenzo <vincenzolucente@gmail.com>
2020-03-27 16:08:23 +0100

    Mockito MVC

--- src/test/java/guru/springframework/controllers/RecipeControllerTest.java
+++ src/test/java/guru/springframework/controllers/RecipeControllerTest.java
@@ -5,6 +5,9 @@ import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -14,6 +17,9 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import guru.springframework.model.Recipe;
@@ -37,6 +43,14 @@ public class RecipeControllerTest {
 
         controller = new RecipeController(recipeService);
     }
+    
+    @Test
+    public void testMockMVC() throws Exception{
+    	MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    	mockMvc.perform(get("/recipes"))
+    	.andExpect(status().isOk())
+    	.andExpect(view().name("recipes/index"));
+    }
 
     @Test
     public void getIndexPage() throws Exception {
@@ -54,7 +68,6 @@ public class RecipeControllerTest {
 
         //when
         String viewName = controller.getRecipeList(model);
-
 
         //then
         assertEquals("recipes/index", viewName);
