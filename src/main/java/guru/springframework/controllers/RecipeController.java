2d11939d085c9c560bedd97c32eb4ee0531f6ecc
Vincenzo <vincenzolucente@gmail.com>
2020-04-03 11:15:46 +0200

    create new recipe

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -2,9 +2,13 @@ package guru.springframework.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import guru.springframework.commands.RecipeCommand;
 import guru.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 
@@ -30,6 +34,18 @@ public class RecipeController {
 	public String showRecipeById(@PathVariable String id, Model model) {
 		model.addAttribute("recipe",service.getRecipeById(new Long(id)));
 		return "recipes/show";
+	}
+	
+	@RequestMapping("/new")
+	public String newRecipe(Model model) {
+		model.addAttribute("recipe", new RecipeCommand());
+		return "recipes/recipeform";
+	}
+	
+	@PostMapping({"/",""})
+	public String saveOrUpdate(@ModelAttribute RecipeCommand command) {
+		RecipeCommand savedCommand = service.saveRecipeCommand(command);
+		return "redirect:/recipes/show/"+savedCommand.getId();
 	}
 		
 }
